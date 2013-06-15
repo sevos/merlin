f5c1b29c74fa6adb7ee2b81a779c75f7166feb40
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-06-15 12:04:47 +0200

    Memoize once evaluated configuration

--- lib/merlin.rb
+++ lib/merlin.rb
@@ -2,23 +2,36 @@ require "merlin/configuration"
 require "merlin/version"
 
 module Merlin
+  def self.__configuration__
+    Merlin::Configuration.new(config_path, env, logger)
+  end
   
   def self.method_missing(*args, &block)
-    logger = @logger || Logger.new(STDOUT)
-    raise "Missing configuration path -> Merlin.config_path = '...'" unless @config_path
-    raise "Missing environment -> Merlin.env = :production" unless @env
-    Merlin::Configuration.new(@config_path, @env, logger).send(*args, &block)
+    raise "Environment not set" unless env
+    __configuration__.send(*args, &block)
   end
 
   def self.config_path=(path)
     @config_path = path  
   end
 
+  def self.config_path
+    @config_path ||= "config/merlin.yml"
+  end
+
   def self.env=(env)
     @env = env
   end
 
+  def self.env
+    @env ||= ENV["RAILS_ENV"]
+  end
+
   def self.logger=(logger)
     @logger = logger
+  end
+  
+  def self.logger
+    @logger ||= Logger.new(STDOUT)
   end
 end
